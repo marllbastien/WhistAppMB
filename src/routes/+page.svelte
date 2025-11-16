01294c62c8364ad6de38dfa47235f7f9b9f3d799
BASTIEN Marlene <Marlene.BASTIEN@aviq.be>
2025-11-16 01:06:52 +0100

    Ajout du logo central + mise à jour page login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -158,7 +158,7 @@
 
   .page-logo-center {
   position: absolute;
-  top: 25%;                    /* 👉 Légèrement plus haut pour ne pas toucher le titre */
+  top: 26%;                    /* 👉 Légèrement plus haut pour ne pas toucher le titre */
   left: 50%;
   transform: translateX(-50%);
   width: 230px;
